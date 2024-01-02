dedcedfa2a6e29aeee5371351d9bdcf10096d0e8
OmarMWarraich <earthling986@gmail.com>
2024-01-02 22:31:39 +0500

    feat: Split Text, get Vectors, Insert into DB

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React, { useState, useEffect } from 'react';
+import OpenAI from 'openai';
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createClient } from '@supabase/supabase-js';
 
 const App = () => {
   const [embedding, setEmbedding] = useState([]);
 
-  async function splitDocument() {
-    const response = await fetch('podcasts.txt');
+  const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+
+  const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+  const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  async function splitDocument(document){
+    const response = await fetch(document);
     const text = await response.text();
-  
+
     const splitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 150,
-      chunkOverlap: 15,
+      chunkSize: 250,
+      chunkOverlap: 35,
     });
     const output = await splitter.createDocuments([text]);
-    console.log(output);
-    setEmbedding(output);
+    return output;
   }
 
+  async function createAndStoreEmbeddings(){
+    const chunkData = await splitDocument("movies.txt");
+    const data = await Promise.all(
+      chunkData.map(async (chunk) => {
+        const embeddingResponse = await openai.embeddings.create({
+          model: "text-embedding-ada-002",
+          input: chunk.pageContent
+        });
+        return { 
+          content: chunk.pageContent, 
+          embedding: embeddingResponse.data[0].embedding 
+        }
+      })
+    );
+    await supabase.from('movies').insert(data);
+    console.log(data);
+    setEmbedding(data);
+  };
+
+  
   useEffect(() => {
     let isMounted = true;
-
+    
     if (isMounted) {
-      splitDocument();
+      createAndStoreEmbeddings();
     }
     
     return () => { isMounted = false };
@@ -34,7 +64,8 @@ const App = () => {
           return (
             <div key={index}>
               <h1>Chunk {index}</h1>
-              <p>{chunk.pageContent}</p>
+              <p>{chunk.content}</p>
+              <p>{chunk.embedding}</p>
             </div>
           );
         })
