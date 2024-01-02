698af43429c403db0d3c60fb6b6197d352e83ea8
OmarMWarraich <earthling986@gmail.com>
2024-01-02 21:53:11 +0500

    feat: Split Document w/ Langchain's CharacterTextSplitter

--- src/App.js
+++ src/App.js
@@ -1,79 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import OpenAI from 'openai';
-import { createClient } from "@supabase/supabase-js";
-import podcasts from './content';
+import { CharacterTextSplitter } from "langchain/text_splitter"
 import './App.css';
 
 const App = () => {
   const [embedding, setEmbedding] = useState([]);
 
-  const openai = new OpenAI({
-    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true,
-  });
-
-  const privateKey = process.env.REACT_APP_SUPABASE_API_KEY;
-  const url = process.env.REACT_APP_SUPABASE_URL;
-  const supabase = createClient(url, privateKey);
-
-  const query = "suggest a podcast for elon musk";
+  async function splitDocument() {
+    const response = await fetch('podcasts.txt');
+    const text = await response.text();
   
-  async function main(input) {
-    const embedding = await createEmbedding(input);
-    const match = await findNearestMatch(embedding);
-    await getChatCompletion(match, input);
-  }
-  
-  async function createEmbedding(input) {
-    const embeddingResponse = await openai.embeddings.create({
-      model: "text-embedding-ada-002",
-      input
+    const splitter = new CharacterTextSplitter({
+      separator: " ",
+      chunkSize: 150,
+      chunkOverlap: 15,
     });
-    return embeddingResponse.data[0].embedding;
-  }
-  
-  async function findNearestMatch(embedding) {
-    const { data } = await supabase.rpc('match_documents', {
-      query_embedding: embedding,
-      match_threshold: 0.50,
-      match_count: 1
-    });
-    return data[0].content;
-  }
-
-  const chatMessages = [{
-    role: 'system',
-    content: `You are an enthusiastic podcast expert who loves recommending podcasts to people. You will be given two pieces of information - some context about podcasts episodes and a question. Your main job is to formulate a short answer to the question using the provided context. If you are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer." Please do not make up the answer.` 
-  }];
-
-  async function getChatCompletion(text, query) {
-    chatMessages.push({
-      role: 'user',
-      content: `Context: ${text} Question: ${query}`
-    });
-    
-    const response = await openai.chat.completions.create({
-      model: 'gpt-4',
-      messages: chatMessages,
-      temperature: 0.5,
-      frequency_penalty: 0.5
-    });
-  
-    console.log(response.choices[0].message.content);
-    setEmbedding(response.choices[0].message.content);
+    const output = await splitter.createDocuments([text]);
+    console.log(output);
+    setEmbedding(output);
   }
 
   useEffect(() => {
-    let isMounted = true;  
-      if (isMounted) {
-        main(query);
-      }
+    let isMounted = true;
+
+    if (isMounted) {
+      splitDocument();
+    }
+    
     return () => { isMounted = false };
   }, []);
-
+  
   return (
     <div className="App">
-      {embedding}
+      {
+        embedding.map((chunk, index) => {
+          return (
+            <div key={index}>
+              <h1>Chunk {index}</h1>
+              <p>{chunk.pageContent}</p>
+            </div>
+          );
+        })
+      }
+        
     </div>
   );
 };
